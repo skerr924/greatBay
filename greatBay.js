7513378ad940baed2091d25a0faae9ce470e2366
Sarah Kerr <sarah.kerr93@gmail.com>
2020-08-10 20:10:37 -0400

    inquirer now loads correctly

--- greatBay.js
+++ greatBay.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var password = require("./pw"); 
+var inquirer = require("inquirer"); 
 
 
 var connection = mysql.createConnection({
@@ -41,6 +42,49 @@ function startingPrompt(){
 }
 
 function postItem(){ 
+
+  // inquirer
+  //   .prompt([
+  //     {
+  //       name: "item",
+  //       type: "input",
+  //       message: "What is the item you would like to submit?"
+  //     },
+  //     {
+  //       name: "category",
+  //       type: "input",
+  //       message: "What category would you like to place your auction in?"
+  //     },
+  //     {
+  //       name: "startingBid",
+  //       type: "input",
+  //       message: "What would you like your starting bid to be?",
+  //       validate: function(value) {
+  //         if (isNaN(value) === false) {
+  //           return true;
+  //         }
+  //         return false;
+  //       }
+  //     }
+  //   ])
+  //   .then(function(answer) {
+  //     // when finished prompting, insert a new item into the db with that info
+  //     connection.query(
+  //       "INSERT INTO auctions SET ?",
+  //       {
+  //         item_name: answer.item,
+  //         category: answer.category,
+  //         starting_bid: answer.startingBid || 0,
+  //         highest_bid: answer.startingBid || 0
+  //       },
+  //       function(err) {
+  //         if (err) throw err;
+  //         console.log("Your auction was created successfully!");
+  //         // re-prompt the user for if they want to bid or post
+  //         start();
+  //       }
+  //     );
+  //   });
 
 }
 
